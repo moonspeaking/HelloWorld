47b429d66cae0f2f17bcfbc4f8ee3607887add0e
Abdallah Abdelazim <abdallah.abdelazim@hotmail.com>
2017-08-31 02:49:01 +0200

    Initial commit

---
+++ app/src/main/java/com/abdallah_abdelazim/helloworld/MainActivity.java
@@ -0,0 +1,65 @@
+package com.abdallah_abdelazim.helloworld;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    public void onClickMeButtonClicked(View view) {
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(getString(R.string.hello_world));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_get_source_code) {
+            openWebPage("https://github.com/Abdallah-Abdelazim/HelloWorld");
+            return true;
+        }
+        else if (id == R.id.action_trainig) {
+            openWebPage("https://developer.android.com/training/index.html");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void openWebPage(String url) {
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+}
